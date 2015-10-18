7c75a2c62eb4ea01f9e9781245493b4f8dd77f33
Restuta <restuta8@gmail.com>
2015-10-17 20:58:45 -0700

    added nanybar

--- timer.js
+++ timer.js
@@ -2,24 +2,26 @@ import chalk from 'chalk';
 import {stdout as logLine} from 'single-line-log'; //stands for "single log"
 import moment from 'moment';
 import {} from 'moment-duration-format';
-
+import notifier from 'node-notifier';
+import nanybar from 'nanybar';
 
 
 let secondsLeft = moment.duration(10, 'seconds');
+nanybar('yellow');
 
 
 var interval = setInterval(() => {
   if (secondsLeft.asSeconds() < 0) {
     clearInterval(interval);
-    process.exit(0);
+    onDone();
+  } else {
+    onEveryTick(secondsLeft);
+    secondsLeft.subtract(1, 'second');
   }
-  runEverySecond(secondsLeft);
-  secondsLeft.subtract(1, 'second');
 }, 1000);
 
 
-function runEverySecond(secondsLeft) {
-
+function onEveryTick(secondsLeft) {
   if (secondsLeft.asSeconds() === 0) {
     logLine(chalk.green('Done!') + '\n'
       + secondsLeft.format('mm:ss') + '\n'
@@ -29,6 +31,30 @@ function runEverySecond(secondsLeft) {
       + secondsLeft.format('mm:ss') + '\n'
     );
   }
-
-
 }
+
+notifier.on('timeout', function (notifierObject, options) {
+  //nanybar('exclamation');
+});
+
+notifier.on('click', function (notifierObject, options) {
+  process.exit(0);
+})
+
+function onDone() {
+  nanybar('green');
+
+  notifier.notify({
+    title: 'Timer',
+    message: 'Done!',
+    sound: true,
+    wait: true
+  });
+
+  //so all notifications are safely processed if nobode reacts on a timer within 5 mins exit anyway
+  setTimeout(() => {
+    process.exit(0);
+  }, 60 * 1000 * 5);
+};
+
+export default {};
