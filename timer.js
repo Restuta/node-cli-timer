d7f01c4f0f504c649a4af8fbf3e64d1e536189aa
Restuta <restuta8@gmail.com>
2015-10-17 20:08:26 -0700

    added basic version of working timer and proper cleaning and formatting

--- timer.js
+++ timer.js
@@ -1,10 +1,34 @@
 import chalk from 'chalk';
+import {stdout as logLine} from 'single-line-log'; //stands for "single log"
+import moment from 'moment';
+import {} from 'moment-duration-format';
 
-console.log(chalk.red('bla'));
-
-var interval = setInterval(everySecond, 1000);
 
 
-function everySecond() {
-  console.log({...arguments});
+let secondsLeft = moment.duration(10, 'seconds');
+
+
+var interval = setInterval(() => {
+  if (secondsLeft.asSeconds() < 0) {
+    clearInterval(interval);
+    process.exit(0);
+  }
+  runEverySecond(secondsLeft);
+  secondsLeft.subtract(1, 'second');
+}, 1000);
+
+
+function runEverySecond(secondsLeft) {
+
+  if (secondsLeft.asSeconds() === 0) {
+    logLine(chalk.green('Done!') + '\n'
+      + secondsLeft.format('mm:ss') + '\n'
+    );
+  } else {
+    logLine(chalk.yellow('Up and counting...') + '\n'
+      + secondsLeft.format('mm:ss') + '\n'
+    );
+  }
+
+
 }
